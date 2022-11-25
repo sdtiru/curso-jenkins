c0380d2f74a2296c83d234a4566c01d830137c2c
Javier <jmartin@grupoloyal.com>
2022-11-25 19:33:43 +0100

    Sin fallos

--- src/main/java/com/gildedrose/Calculadora.java
+++ src/main/java/com/gildedrose/Calculadora.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 public class Calculadora {
 	public double suma(double a, double b) {
 		BigDecimal rslt = BigDecimal.valueOf(a + b);
-//		return rslt.setScale(15, java.math.RoundingMode.HALF_DOWN).doubleValue();
-		return a + b;
+		return rslt.setScale(15, java.math.RoundingMode.HALF_DOWN).doubleValue();
 	}
 }
